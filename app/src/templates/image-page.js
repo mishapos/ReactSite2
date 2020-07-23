8a069a2a3f99bbcfbcb8fb3498c12f33737b4ea3
Misha Shaposhnikov <mishapos@microsoft.com>
2020-07-23 14:44:35 -0700

    Init commit

---
+++ app/src/templates/image-page.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default function ImagePage({ data }) {
+  const post = data.file
+  return (
+    <Layout>
+      <div>
+        <img src={post.publicURL} alt="NASA space" />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($name: String!) {
+    file(name: { eq: $name }) {
+      id
+      publicURL
+    }
+  }
+`
